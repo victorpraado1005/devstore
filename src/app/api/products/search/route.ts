237d47b2589646822d780dc8821e8db81907a3e0
Victor Prado <prado.victor1005@gmail.com>
2024-07-26 00:14:41 -0300

    buscando produtos na API

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -3,13 +3,13 @@ import type { NextRequest } from 'next/server'
 import data from '../data.json'
 
 export async function GET(request: NextRequest) {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 3000))
 
   const { searchParams } = request.nextUrl
 
   const query = z.string().parse(searchParams.get('q'))
 
-  const products = data.products.find((product) => {
+  const products = data.products.filter((product) => {
     return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
   })
 
